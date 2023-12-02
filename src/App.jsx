1ebccba2138cef93e1ccae7bc370318ac0cadaf1
sabeet <sabeet.other.way@gmail.com>
2023-12-02 10:38:22 -0500

    reaaranged div

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,7 @@ function App() {
       {quotes.id}
       <div className="text-4xl"> &#125; </div> */}
 
-      <div className="text-2xl"> Is this raw json? No, it's actually a creative way for me to practice using the Fetch API in React. Refresh the page to see it change.</div>
-
-      <div className="text-4xl p-1"> &#123; </div>
+      <div className="text-xl p-2"> &#123; </div>
       <div className="flex flex-row">
         <div> &nbsp; </div>
         <div class="grid grid-cols-3 divide-x">
@@ -32,16 +30,48 @@ function App() {
           <div>&nbsp;</div>
         </div>
         <div className="flex flex-col col-span-6">
-          <div>"id" : "{quotes.id}"</div>
-          <div>"English" : "{quotes.quote_english}"</div>
-          <div>"Bangla" : "{quotes.quote_bangla}"</div>
-          <div>"Spanish" : "{quotes.quote_spanish}"</div>
-          <div>"Arabic" : "{quotes.quote_arabic}"</div>
-          <div>"Hindi" : "{quotes.quote_hindi}"</div>
-          <div>"Japanese" : "{quotes.quote_japanese}"</div>
+          <div className="flex flex-row">
+            <div>"id" : "</div>
+            <div className="text-green-400">{quotes.id}"</div>
+          </div>
+          <div className="flex flex-row">
+            <div>"English" : </div>"
+            <div className="text-green-400">{quotes.quote_english}"</div>,
+          </div>
+          <div className="flex flex-row">
+            <div>"Bangla" : </div>"
+            <div className="text-green-400">{quotes.quote_bangla}"</div>,
+          </div>
+          <div className="flex flex-row">
+            <div>"Spanish" : </div>"
+            <div className="text-green-400">{quotes.quote_spanish}"</div>,
+          </div>
+          <div className="flex flex-row">
+            <div>"Arabic" : </div>"
+            <div className="text-green-400">{quotes.quote_arabic}"</div>,
+          </div>
+          <div className="flex flex-row">
+            <div>"Hindi" : </div>"
+            <div className="text-green-400">{quotes.quote_hindi}"</div>,
+          </div>
+          <div className="flex flex-row">
+            <div>"Japanese" : </div>"
+            <div className="text-green-400">{quotes.quote_japanese}"</div>
+          </div>
         </div>
       </div>
-      <div className="text-4xl"> &#125; </div>
+      <div className="text-xl p-2"> &#125; </div>
+
+      <div className="flex flex-row h-12 p-4 lg:text-2xl">
+        <div className="w-1/2">
+          {" "}
+          Is this parsed json? No, it's actually a creative way for me to
+          practice using the Fetch API in React. Refresh the page to see it
+          change.
+        </div>
+
+        <div className="w-1/2 hidden">left blank intentionally</div>
+      </div>
     </>
   );
 }
