e457b8fa92982a798a101461cee45645006f524c
sabeet <sabeet.other.way@gmail.com>
2023-12-03 14:43:28 -0500

    updated api so that i can be locally hosted

--- src/DataService.js
+++ src/DataService.js
@@ -1,6 +1,6 @@
 export async function getQuotes(id) {
   const response = await fetch(
-    `https://quote-machine-h8o8.onrender.com/quotes/quotes/${id}`
+    `https://considerable-moves-includes-boston.trycloudflare.com/quotes/quotes/${id}`
   );
   const data = await response.json();
 
