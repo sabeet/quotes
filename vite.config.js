645150203f2ff5c4c6fac7cbf07a62a2fa9ed5ba
sabeet <sabeet.other.way@gmail.com>
2023-12-02 22:36:00 -0500

    frontend finished

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "https://quote-machine-h8o8.onrender.com",
+    },
+  },
   plugins: [react()],
-})
+});
